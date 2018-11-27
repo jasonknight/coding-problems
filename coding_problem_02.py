984ea54c57ab6155e7f9e76e099d0260c0c84a56
Jason Knight <jolierouge242@gmail.com>
2018-11-27 21:17:13 +0100

    adding in faster algo for #2

--- coding_problem_02.py
+++ coding_problem_02.py
@@ -31,8 +31,33 @@ def prod_without_division(inp):
             if not i == j:
                 res[i] *= inp[j]
     return list(res.values())
+# Hrrm, can we do it in near linear time, i mean N*N is not 
+# really nice
+def prod_fast(inp):
+    res = []
+    left = []
+    right = []
+    lt = 1
+    rt = 1
+    rindex = len(inp) - 1
+    for i in range(len(inp)):
+        lt *= inp[i]
+        left.append(lt)
+        rt *= inp[rindex]
+        right.insert(0,rt)
+        rindex -= 1
+    res.append(right[1])
+    i = 1
+    while i < len(inp) - 1:
+        # notice we skip 1 on the right side!
+        res.append(left[i - 1] * right[i + 1])
+        i += 1
+    res.append(left[i - 1])
+    return res
 
 print(prod([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24])
 print(prod([3, 2, 1]) == [2, 3, 6])
 print(prod_without_division([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24])
 print(prod_without_division([3, 2, 1]) == [2, 3, 6])
+print(prod_fast([1, 2, 3, 4, 5]) == [120, 60, 40, 30, 24])
+print(prod_fast([3, 2, 1]) == [2, 3, 6])
