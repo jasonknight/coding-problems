17f7d26df9b1f88d1c85786609ec5536c9944c17
Jason Knight <jolierouge242@gmail.com>
2018-12-02 13:13:21 +0100

    Adding in longest distinct substring

---
+++ coding_problem_13.py
@@ -0,0 +1,57 @@
+# We need to find the substring with at most k distinct, or unique, characters.
+# 
+# For me, this was a little confusing at first, but if we restate it a little
+# different it becomes clear.
+# 
+# If k = 2, then what we are looking for is the longest possible substring
+# that only has 2 characters. So if you have s = "abcba", then "bcb" is the
+# longest substring, because it only contains b and c (k=2). If s had
+# equaled "abcbcbbcca", then the longest would have been: "bcbcbbcc".
+
+# Let's try first with a brute force solution, this one is
+# not too bad, O(n^2*k)
+
+def longest_distinct_substring(s,k):
+    longest = ''
+    for i,c in enumerate(s):
+        # making j bigger than s, ensures we'll get to the end of
+        # of the string. Going over is okay.
+        for j in range(i + 1, len(s) + 1):
+            sub = s[i:j]
+            # converting it to a set gives
+            # gives us unique elements
+            sub_set = set(sub)
+            if len(sub_set) <= k and len(sub) > len(longest):
+                longest = sub
+    return len(longest)
+
+# Another way to look at this problem is tracking a "window", that
+# is, just keeping track of the start position and end position
+#  (0,0) (0,1) (0,2)
+#    0     1     2     3     4
+#    a     b     c     b     a
+#                OOPs, more than k unique!
+#        (1,1) (1,2) (1,3) (1,4)
+#    0     1     2     3     4
+#    a     b     c     b     a
+#                            OOPs, more than k unique!
+#   final_window is (1,3) = 3 for "bcb"
+# 
+def longest_distinct_substring_fast(s,k):
+    window = (0,0)
+    memo = {}
+    length = 0
+    for i,c in enumerate(s):
+        memo[c] = i # need to track the "start" offset
+        new_start_position = window[0]
+        if len(memo) > k:
+            #pop the last occurrence
+            key = min(memo,key=memo.get)
+            new_start_position = memo.pop(key) + 1 # i.e. advance one position past.
+            # remember, we store i at the key, 
+        # we advance the window on each iteration
+        window = (new_start_position, window[1] + 1) 
+        length = max(length, window[1] - window[0])
+    return length
+print(longest_distinct_substring("abcba",2))
+print(longest_distinct_substring_fast("abcba",2))
